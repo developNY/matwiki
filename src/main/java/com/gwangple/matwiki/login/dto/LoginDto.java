e443170ba8ec84f5b50636971ecce29f866ea46d
pakooon <rapbby@naver.com>
2017-06-25 15:44:18 +0900

    login xml 업로드

---
+++ src/main/java/com/gwangple/matwiki/login/dto/LoginDto.java
@@ -0,0 +1,29 @@
+package com.gwangple.matwiki.login.dto;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+public class LoginDto {
+	
+	@NotEmpty
+	private String userId;
+	@NotEmpty
+	private String password;
+	
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	@Override
+	public String toString() {
+		return "LoginDto [userId=" + userId + ", password=" + password + "]";
+	}
+	
+}
