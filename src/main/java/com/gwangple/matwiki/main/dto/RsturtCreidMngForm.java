a0f0d6e7157c37f918761802b2d26b3aa8b64b74
ppelo1 <ppelo@naver.com>
2017-08-17 10:55:39 +0900

    맛집등록, 채번 DTO 추가

---
+++ src/main/java/com/gwangple/matwiki/main/dto/RsturtCreidMngForm.java
@@ -0,0 +1,91 @@
+package com.gwangple.matwiki.main.dto;
+
+public class RsturtCreidMngForm {
+	private String rsturtMngId = "";
+	private String rsturtNm =  "";
+	private String addr =  "";
+	private String rsturtId = "";
+	private String tel1 =  "";
+	private String tel2 =  "";
+	private String tel3 =  "";
+	private String tel =  "";
+	private String createId = "";
+	private String uptId = "";
+	private String createDt = "";
+	private String uptDt = "";
+	
+	public String getRsturtMngId() {
+		return rsturtMngId;
+	}
+	public void setRsturtMngId(String rsturtMngId) {
+		this.rsturtMngId = rsturtMngId;
+	}
+	public String getRsturtNm() {
+		return rsturtNm;
+	}
+	public void setRsturtNm(String rsturtNm) {
+		this.rsturtNm = rsturtNm;
+	}
+	public String getAddr() {
+		return addr;
+	}
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+	public String getRsturtId() {
+		return rsturtId;
+	}
+	public void setRsturtId(String rsturtId) {
+		this.rsturtId = rsturtId;
+	}
+	public String getTel1() {
+		return tel1;
+	}
+	public void setTel1(String tel1) {
+		this.tel1 = tel1;
+	}
+	public String getTel2() {
+		return tel2;
+	}
+	public void setTel2(String tel2) {
+		this.tel2 = tel2;
+	}
+	public String getTel3() {
+		return tel3;
+	}
+	public void setTel3(String tel3) {
+		this.tel3 = tel3;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getCreateId() {
+		return createId;
+	}
+	public void setCreateId(String createId) {
+		this.createId = createId;
+	}
+	public String getUptId() {
+		return uptId;
+	}
+	public void setUptId(String uptId) {
+		this.uptId = uptId;
+	}
+	public String getCreateDt() {
+		return createDt;
+	}
+	public void setCreateDt(String createDt) {
+		this.createDt = createDt;
+	}
+	public String getUptDt() {
+		return uptDt;
+	}
+	public void setUptDt(String uptDt) {
+		this.uptDt = uptDt;
+	}
+	
+	
+}
---
+++ src/main/java/com/gwangple/matwiki/main/dto/RsturtInfoForm.java
@@ -0,0 +1,110 @@
+package com.gwangple.matwiki.main.dto;
+
+public class RsturtInfoForm {
+	private String rsturtMngId = "";
+	private String addrSeq = "";
+	private String bizStrTime = "";
+	private String telNo = "";
+	private String rnk = "";
+	private String priceRngStr =""; 
+	private String createDt = "";
+	private String createId = "";
+	private String uptId =""; 
+	private String uptDt =""; 
+	private String bizEndTime =""; 
+	private String priceRngEnd = "";
+	private String rmdAddr = "";
+	private String rsturtId =""; 
+	private String addr = "";
+	
+	public String getRsturtMngId() {
+		return rsturtMngId;
+	}
+	public void setRsturtMngId(String rsturtMngId) {
+		this.rsturtMngId = rsturtMngId;
+	}
+	public String getAddrSeq() {
+		return addrSeq;
+	}
+	public void setAddrSeq(String addrSeq) {
+		this.addrSeq = addrSeq;
+	}
+	public String getBizStrTime() {
+		return bizStrTime;
+	}
+	public void setBizStrTime(String bizStrTime) {
+		this.bizStrTime = bizStrTime;
+	}
+	public String getTelNo() {
+		return telNo;
+	}
+	public void setTelNo(String telNo) {
+		this.telNo = telNo;
+	}
+	public String getRnk() {
+		return rnk;
+	}
+	public void setRnk(String rnk) {
+		this.rnk = rnk;
+	}
+	public String getPriceRngStr() {
+		return priceRngStr;
+	}
+	public void setPriceRngStr(String priceRngStr) {
+		this.priceRngStr = priceRngStr;
+	}
+	public String getCreateDt() {
+		return createDt;
+	}
+	public void setCreateDt(String createDt) {
+		this.createDt = createDt;
+	}
+	public String getCreateId() {
+		return createId;
+	}
+	public void setCreateId(String createId) {
+		this.createId = createId;
+	}
+	public String getUptId() {
+		return uptId;
+	}
+	public void setUptId(String uptId) {
+		this.uptId = uptId;
+	}
+	public String getUptDt() {
+		return uptDt;
+	}
+	public void setUptDt(String uptDt) {
+		this.uptDt = uptDt;
+	}
+	public String getBizEndTime() {
+		return bizEndTime;
+	}
+	public void setBizEndTime(String bizEndTime) {
+		this.bizEndTime = bizEndTime;
+	}
+	public String getPriceRngEnd() {
+		return priceRngEnd;
+	}
+	public void setPriceRngEnd(String priceRngEnd) {
+		this.priceRngEnd = priceRngEnd;
+	}
+	public String getRmdAddr() {
+		return rmdAddr;
+	}
+	public void setRmdAddr(String rmdAddr) {
+		this.rmdAddr = rmdAddr;
+	}
+	public String getRsturtId() {
+		return rsturtId;
+	}
+	public void setRsturtId(String rsturtId) {
+		this.rsturtId = rsturtId;
+	}
+	public String getAddr() {
+		return addr;
+	}
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+}
